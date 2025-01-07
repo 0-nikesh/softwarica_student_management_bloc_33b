4cc8276f2b9a1a61a46a529c3bed0351b81a1c38
nikesh <bhandarinikesh93@gmail.com>
2025-01-07 23:04:32 +0545

    as per the thing

--- lib/features/batch/data/data_source/batch_data_source.dart
+++ lib/features/batch/data/data_source/batch_data_source.dart
@@ -1,9 +1,7 @@
-import 'package:dartz/dartz.dart';
-import 'package:softwarica_student_management_bloc/core/error/failure.dart';
-import 'package:softwarica_student_management_bloc/features/course/domain/entity/course_entity.dart';
+import 'package:softwarica_student_management_bloc/features/batch/domain/entity/batch_entity.dart';
 
-abstract interface class iBatchDataSource {
-  Future<Either<Failure, List<CourseEntity>>> getBatches();
-  Future<Either<Failure, void>> createBatch(CourseEntity batch);
-  Future<Either<Failure, void>> deleteBatch(String id);
+abstract interface class IBatchRepository {
+  Future<List<BatchEntity>> getBatches();
+  Future<void> createBatch(BatchEntity batch);
+  Future<void> deleteBatch(String id);
 }
