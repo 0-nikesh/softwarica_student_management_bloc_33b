07f64e6c647ebbe5579e53dcc25c3509b4be1dc7
nikesh <bhandarinikesh93@gmail.com>
2025-01-07 22:03:30 +0545

    first commit

---
+++ lib/features/course/domain/repository/course_repository.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:softwarica_student_management_bloc/core/error/failure.dart';
+import 'package:softwarica_student_management_bloc/features/course/domain/entity/course_entity.dart';
+
+abstract interface class iCourseRepository {
+  Future<Either<Failure, List<CourseEntity>>> getBatches();
+  Future<Either<Failure, void>> createBatch(CourseEntity batch);
+  Future<Either<Failure, void>> deleteBatch(String id);
+}
